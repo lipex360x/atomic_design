2b18e2758979df10eb9795b0b804d2cf3e3a4f6c
Felipe Borges <lipex360@gmail.com>
2021-11-24 21:59:59 -0300

    snnipets added

---
+++ src/components/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <section></section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<style lang="scss" scoped>
+section {
+}
+</style>
